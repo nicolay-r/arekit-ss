66ec4503fba02937efe727270a01ba7baa3c0b76
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-05-25 00:26:33 +0100

    Fixed RuAttitudes unit tests

--- test/test_arekit_ruattitudes.py
+++ test/test_arekit_ruattitudes.py
@@ -1,11 +1,15 @@
 import unittest
+
+from arekit.common.pipeline.base import BasePipeline
 from arekit.contrib.utils.data.writers.json_opennre import OpenNREJsonWriter
 
-from framework.arekit.bert_sampler import create_bert_sampler
+from framework.arekit.rows_bert import create_bert_rows_provider
+from framework.arekit.rows_nn import create_nn_rows_provider
 from framework.arekit.serialize_bert import serialize_bert_pipeline
 from framework.arekit.serialize_nn import serialize_nn_pipeline
 from sources.config import SourcesConfig
 from sources.s_rusentrel import build_datapipeline_bert, build_datapipeline_nn
+from sources.scaler import PosNegNeuRelationsLabelScaler
 
 
 class TestRuAttitudes(unittest.TestCase):
@@ -20,9 +24,12 @@ class TestRuAttitudes(unittest.TestCase):
 
     def test_serialize_bert_opennre(self):
         data_folding, pipelines = build_datapipeline_bert(self.__config())
-        s_ppl = serialize_bert_pipeline(output_dir="_out/ra-bert",
-                                        writer=OpenNREJsonWriter(text_columns=["text_a"]),
-                                        sample_row_provider=create_bert_sampler(100))
+        item = serialize_bert_pipeline(output_dir="_out/ra-bert",
+                                       writer=OpenNREJsonWriter(text_columns=["text_a"]),
+                                       rows_provider=create_bert_rows_provider(
+                                           terms_per_context=100,
+                                           labels_scaler=PosNegNeuRelationsLabelScaler()))
+        s_ppl = BasePipeline([item])
         s_ppl.run(input_data=None,
                   params_dict={
                       "data_folding": data_folding,
@@ -31,8 +38,11 @@ class TestRuAttitudes(unittest.TestCase):
 
     def test_serialize_nn_csv(self):
         data_folding, pipelines = build_datapipeline_nn(self.__config())
-        s_ppl = serialize_nn_pipeline(output_dir="_out/ra-nn",
-                                      writer=OpenNREJsonWriter(text_columns=["text_a"]))
+        item = serialize_nn_pipeline(output_dir="_out/ra-nn",
+                                     writer=OpenNREJsonWriter(text_columns=["text_a"]),
+                                     rows_provider=create_nn_rows_provider(
+                                         labels_scaler=PosNegNeuRelationsLabelScaler()))
+        s_ppl = BasePipeline([item])
         s_ppl.run(input_data=None,
                   params_dict={
                       "data_folding": data_folding,
@@ -41,9 +51,12 @@ class TestRuAttitudes(unittest.TestCase):
 
     def test_serialize_nn_opennre(self):
         data_folding, pipelines = build_datapipeline_nn(self.__config())
-        s_ppl = serialize_nn_pipeline(writer=OpenNREJsonWriter(text_columns=["text_a"]),
-                                      output_dir="_out/ra-nn")
+        item = serialize_nn_pipeline(writer=OpenNREJsonWriter(text_columns=["text_a"]),
+                                     output_dir="_out/ra-nn",
+                                     rows_provider=create_nn_rows_provider(
+                                         labels_scaler=PosNegNeuRelationsLabelScaler()))
 
+        s_ppl = BasePipeline([item])
         s_ppl.run(input_data=None,
                   params_dict={
                       "data_folding": data_folding,
