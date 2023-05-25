265b822e9d0343d633a0b6f007d4d2334deea494
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-05-25 11:33:42 +0100

    Refactored unittests

--- test/test_arekit_rusentrel.py
+++ test/test_arekit_rusentrel.py
@@ -1,20 +1,64 @@
 import unittest
-from arekit.contrib.utils.data.writers.csv_pd import PandasCsvWriter
+
+from arekit.common.pipeline.base import BasePipeline
+from arekit.contrib.utils.data.writers.csv_native import NativeCsvWriter
 from arekit.contrib.utils.data.writers.json_opennre import OpenNREJsonWriter
 
-from sources.s_rusentrel import do_serialize_bert, do_serialize_nn
+from framework.arekit.rows_bert import create_bert_rows_provider
+from framework.arekit.rows_nn import create_nn_rows_provider
+from framework.arekit.serialize_bert import serialize_bert_pipeline
+from framework.arekit.serialize_nn import serialize_nn_pipeline
+from sources.config import SourcesConfig
+from sources.s_rusentrel import build_datapipeline_bert
+from sources.s_sentinerel import build_datapipeline_nn
+from sources.scaler import PosNegNeuRelationsLabelScaler
 
 
 class TestRuSentRel(unittest.TestCase):
 
+    def __config(self):
+        cfg = SourcesConfig()
+        cfg.docs_limit = 5
+        cfg.dest_lang = "ru"
+        return cfg
+
     def test_serialize_bert_opennre(self):
-        do_serialize_bert(writer=OpenNREJsonWriter(text_columns=["text_a", "text_b"]),
-                          output_dir="_out/rsr_bert")
+        data_folding, pipelines = build_datapipeline_bert(self.__config())
+        item = serialize_bert_pipeline(output_dir="_out/rsr_bert",
+                                       writer=OpenNREJsonWriter(text_columns=["text_a"]),
+                                       rows_provider=create_bert_rows_provider(
+                                           terms_per_context=100,
+                                           labels_scaler=PosNegNeuRelationsLabelScaler()))
+        pipeline = BasePipeline([item])
+        pipeline.run(input_data=None,
+                     params_dict={
+                      "data_folding": data_folding,
+                      "data_type_pipelines": pipelines
+                     })
 
     def test_serialize_nn_csv(self):
-        do_serialize_nn(writer=PandasCsvWriter(write_header=True),
-                        output_dir="_out/rsr_nn")
+        data_folding, pipelines = build_datapipeline_nn(self.__config())
+        item = serialize_nn_pipeline(output_dir="_out/rsr_nn",
+                                     writer=NativeCsvWriter(),
+                                     rows_provider=create_nn_rows_provider(
+                                         labels_scaler=PosNegNeuRelationsLabelScaler()))
+        s_ppl = BasePipeline([item])
+        s_ppl.run(input_data=None,
+                  params_dict={
+                      "data_folding": data_folding,
+                      "data_type_pipelines": pipelines
+                  })
 
     def test_serialize_nn_opennre(self):
-        do_serialize_nn(writer=OpenNREJsonWriter(text_columns=["text_a"]),
-                        output_dir="_out/rsr_nn")
+        data_folding, pipelines = build_datapipeline_nn(self.__config())
+        item = serialize_nn_pipeline(writer=OpenNREJsonWriter(text_columns=["text_a"]),
+                                     output_dir="_out/rsr-nn",
+                                     rows_provider=create_nn_rows_provider(
+                                         labels_scaler=PosNegNeuRelationsLabelScaler()))
+
+        s_ppl = BasePipeline([item])
+        s_ppl.run(input_data=None,
+                  params_dict={
+                      "data_folding": data_folding,
+                      "data_type_pipelines": pipelines
+                  })
