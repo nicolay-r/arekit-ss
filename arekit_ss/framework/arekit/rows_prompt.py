2b28743e38760d6de684af67ccbc2cd2229f7142
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-07-16 14:53:49 +0100

    #47 added textual formatter

--- arekit_ss/framework/arekit/rows_prompt.py
+++ arekit_ss/framework/arekit/rows_prompt.py
@@ -3,12 +3,12 @@ from arekit.contrib.bert.terms.mapper import BertDefaultStringTextTermsMapper
 from arekit.contrib.prompt.sample import PromptedSampleRowProvider
 from arekit.contrib.utils.entities.formatters.str_display import StringEntitiesDisplayValueFormatter
 
-
-def create_prompt_rows_provider(prompt, labels_scaler):
+def create_prompt_rows_provider(prompt, labels_scaler, labels_formatter):
     return PromptedSampleRowProvider(
         prompt=prompt,
         crop_window_size=200,
         label_scaler=labels_scaler,
         text_provider=BaseSingleTextProvider(
             text_terms_mapper=BertDefaultStringTextTermsMapper(
-                entity_formatter=StringEntitiesDisplayValueFormatter())))
+                entity_formatter=StringEntitiesDisplayValueFormatter())),
+        label_fmt=labels_formatter)
---
+++ arekit_ss/sources/labels/formatter.py
@@ -0,0 +1,12 @@
+from arekit.common.labels.str_fmt import StringLabelsFormatter
+from arekit_ss.sources.labels.sentiment import PositiveTo, NegativeTo
+
+
+class PosNegLabelsFormatter(StringLabelsFormatter):
+
+    def __init__(self):
+        stol = {
+            "negative": PositiveTo(),
+            "positive": NegativeTo()
+        }
+        super(PosNegLabelsFormatter, self).__init__(stol=stol)
