3932352a30632262bcad24df83d44e744569fd37
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-07-16 14:53:49 +0100

    #47 added textual formatter

--- arekit_ss/sample.py
+++ arekit_ss/sample.py
@@ -12,6 +12,7 @@ from arekit_ss.framework.arekit.serialize_bert import serialize_bert_pipeline
 from arekit_ss.framework.arekit.serialize_nn import serialize_nn_pipeline
 from arekit_ss.sources import src_list
 from arekit_ss.sources.config import SourcesConfig
+from arekit_ss.sources.labels.formatter import PosNegLabelsFormatter
 from arekit_ss.sources.labels.scaler import PosNegNeuRelationsLabelScaler
 from arekit_ss.text_parser.text_lm import create_lm
 from arekit_ss.text_parser.text_nn_frames import create_nn_frames
@@ -83,7 +84,9 @@ if __name__ == '__main__':
         pipeline_item = serialize_bert_pipeline(
             output_dir=args.output_dir, writer=writer,
             rows_provider=create_prompt_rows_provider(
-                prompt=args.prompt, labels_scaler=labels_scaler))
+                prompt=args.prompt,
+                labels_scaler=labels_scaler,
+                labels_formatter=PosNegLabelsFormatter()))
 
     # Launch pipeline.
     pipeline = BasePipeline([pipeline_item])
