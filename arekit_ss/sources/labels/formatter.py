2f6020a8695a7622a18d5ffd8e32eeed5734310c
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-07-16 15:17:19 +0100

    Fixed bug #47

--- arekit_ss/sources/labels/formatter.py
+++ arekit_ss/sources/labels/formatter.py
@@ -1,13 +1,15 @@
+from arekit.common.labels.base import NoLabel
 from arekit.common.labels.str_fmt import StringLabelsFormatter
 
 from arekit_ss.sources.labels.sentiment import PositiveTo, NegativeTo
 
 
-class PosNegLabelsFormatter(StringLabelsFormatter):
+class PosNegNeuLabelsFormatter(StringLabelsFormatter):
 
     def __init__(self):
         stol = {
             "negative": PositiveTo,
-            "positive": NegativeTo
+            "positive": NegativeTo,
+            "no-label": NoLabel
         }
-        super(PosNegLabelsFormatter, self).__init__(stol=stol)
+        super(PosNegNeuLabelsFormatter, self).__init__(stol=stol)
