52bc4a466b0089728f916c2f4b63145247bac0cf
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-08-08 10:55:10 +0100

    #63 now we consider to keep labels for any split (0.23.1)

--- arekit_ss/framework/arekit/serialize_nn.py
+++ arekit_ss/framework/arekit/serialize_nn.py
@@ -1,5 +1,4 @@
 from arekit.common.data.const import ENTITIES, ENTITY_TYPES
-from arekit.common.experiment.data_type import DataType
 from arekit.contrib.networks.input.const import FrameVariantIndices, FrameConnotations
 from arekit.contrib.utils.data.storages.row_cache import RowCacheStorage
 from arekit.contrib.utils.data.writers.base import BaseWriter
@@ -23,5 +22,5 @@ def serialize_nn_pipeline(output_dir, writer, rows_provider):
         samples_io=SamplesIO(target_dir=output_dir, writer=writer),
         emb_io=NpEmbeddingIO(target_dir=output_dir),
         balance_func=lambda _: False,
-        save_labels_func=lambda data_type: data_type != DataType.Test,
+        save_labels_func=lambda _: True,
         save_embedding=True)
