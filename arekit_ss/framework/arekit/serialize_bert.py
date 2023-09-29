c620470308777b0c99275a976b7c58b5f4018c99
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-09-29 14:38:23 +0100

    #53 sync with AREkit updates

--- arekit_ss/framework/arekit/serialize_bert.py
+++ arekit_ss/framework/arekit/serialize_bert.py
@@ -3,14 +3,14 @@ from arekit.common.data.input.providers.rows.samples import BaseSampleRowProvide
 from arekit.contrib.utils.data.storages.row_cache import RowCacheStorage
 
 from arekit.contrib.utils.io_utils.samples import SamplesIO
-from arekit.contrib.utils.pipelines.items.sampling.bert import BertExperimentInputSerializerPipelineItem
+from arekit.contrib.utils.pipelines.items.sampling.base import BaseSerializerPipelineItem
 
 
 def serialize_bert_pipeline(writer, rows_provider, output_dir):
     assert(isinstance(rows_provider, BaseSampleRowProvider))
     assert(isinstance(output_dir, str))
 
-    return BertExperimentInputSerializerPipelineItem(
+    return BaseSerializerPipelineItem(
         storage=RowCacheStorage(force_collect_columns=[ENTITIES, ENTITY_TYPES]),
         samples_io=SamplesIO(target_dir=output_dir, writer=writer),
         save_labels_func=lambda _: True,
